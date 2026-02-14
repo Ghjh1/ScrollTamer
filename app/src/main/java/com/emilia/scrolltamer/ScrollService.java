f3742dc2660b5398ed1fcab8f2fa55b41728a083
Ghjh1 <ghjhifyu@gmail.com>
2026-02-14 03:15:37 +0200

    Пустой сервис v10 - проверка регистрации

--- app/src/main/java/com/emilia/scrolltamer/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/ScrollService.java
@@ -1,27 +1,10 @@
 package com.emilia.scrolltamer;
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.GestureDescription;
-import android.graphics.Path;
 import android.view.accessibility.AccessibilityEvent;
 
 public class ScrollService extends AccessibilityService {
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Здесь мы ловим прокрутку от системы
-        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_SCROLLED) {
-             // Можно добавить логику фильтрации, но пока просто даем скомпилироваться
-        }
-    }
-
+    public void onAccessibilityEvent(AccessibilityEvent event) {}
     @Override
     public void onInterrupt() {}
-
-    private void smoothScroll(boolean up) {
-        Path path = new Path();
-        path.moveTo(500, 500);
-        path.lineTo(500, up ? 600 : 400);
-        GestureDescription.Builder builder = new GestureDescription.Builder();
-        builder.addStroke(new GestureDescription.StrokeDescription(path, 1, 100));
-        dispatchGesture(builder.build(), null, null);
-    }
 }
