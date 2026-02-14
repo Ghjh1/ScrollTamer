58af5e63dae19fc824ce597f79b723ac9381f761
Ghjh1 <ghjhifyu@gmail.com>
2026-02-14 13:06:11 +0200

    Оживляю сервис v13: onServiceConnected

--- app/src/main/java/com/emilia/scrolltamer/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/ScrollService.java
@@ -1,10 +1,19 @@
 package com.emilia.scrolltamer;
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.view.accessibility.AccessibilityEvent;
 
 public class ScrollService extends AccessibilityService {
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {}
-    @Override
-    public void onInterrupt() {}
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+        info.notificationTimeout = 100;
+        this.setServiceInfo(info);
+    }
+
+    @Override public void onAccessibilityEvent(AccessibilityEvent event) {}
+    @Override public void onInterrupt() {}
 }
