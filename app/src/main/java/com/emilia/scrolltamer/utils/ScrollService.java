d57b78f70bdd6011dfe19e5fdbad1dfc0bac46d7
Ghjh1 <ghjhifyu@gmail.com>
2026-02-15 01:41:03 +0200

    Silk Touch v31: Проверка клика

--- app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
@@ -1,8 +1,9 @@
 package com.emilia.scrolltamer.utils;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.graphics.Path;
 import android.view.accessibility.AccessibilityEvent;
-import android.util.Log;
 import android.widget.Toast;
 
 public class ScrollService extends AccessibilityService {
@@ -10,15 +11,25 @@ public class ScrollService extends AccessibilityService {
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        // Этот текст ДОЛЖЕН появиться сразу, как ты включишь сервис в настройках
-        Toast.makeText(this, "СЕРВИС ПОДКЛЮЧЕН!", Toast.LENGTH_LONG).show();
-        Log.d("ScrollTamer", "Service Connected!");
+        Toast.makeText(this, "СЕРВИС ОЖИЛ!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Ловим абсолютно любое событие
-        Log.d("ScrollTamer", "Event received: " + event.getEventType());
+        // Проверяем клик по экрану
+        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
+            Toast.makeText(this, "Вижу клик! Скроллю...", Toast.LENGTH_SHORT).show();
+            performAction();
+        }
+    }
+
+    private void performAction() {
+        Path path = new Path();
+        path.moveTo(540, 1500);
+        path.lineTo(540, 500);
+
+        GestureDescription.StrokeDescription stroke = new GestureDescription.StrokeDescription(path, 0, 500);
+        dispatchGesture(new GestureDescription.Builder().addStroke(stroke).build(), null, null);
     }
 
     @Override
