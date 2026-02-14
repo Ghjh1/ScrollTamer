910948ef5072e8edad3c1e37f1e84de5730b69c7
Ghjh1 <ghjhifyu@gmail.com>
2026-02-15 01:19:56 +0200

    Silk Touch v30: Проверка подключения сервиса

--- app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
@@ -1,32 +1,24 @@
 package com.emilia.scrolltamer.utils;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.GestureDescription;
-import android.graphics.Path;
 import android.view.accessibility.AccessibilityEvent;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ScrollService extends AccessibilityService {
 
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Реагируем на любой "чих" системы
-        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ||
-            event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-
-            // Если код работает, ты увидишь это сообщение
-            Toast.makeText(this, "Сервис видит экран!", Toast.LENGTH_SHORT).show();
-            performSimpleScroll();
-        }
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        // Этот текст ДОЛЖЕН появиться сразу, как ты включишь сервис в настройках
+        Toast.makeText(this, "СЕРВИС ПОДКЛЮЧЕН!", Toast.LENGTH_LONG).show();
+        Log.d("ScrollTamer", "Service Connected!");
     }
 
-    private void performSimpleScroll() {
-        Path path = new Path();
-        path.moveTo(500, 1000);
-        path.lineTo(500, 500);
-
-        GestureDescription.StrokeDescription stroke = new GestureDescription.StrokeDescription(path, 0, 500);
-        dispatchGesture(new GestureDescription.Builder().addStroke(stroke).build(), null, null);
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // Ловим абсолютно любое событие
+        Log.d("ScrollTamer", "Event received: " + event.getEventType());
     }
 
     @Override
