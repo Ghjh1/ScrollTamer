272cba952a60bd9de7c815189541f90d8df281fa
Ghjh1 <ghjhifyu@gmail.com>
2026-02-15 17:41:29 +0200

    v39: От наблюдения к управлению жестами

--- app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
@@ -1,6 +1,8 @@
 package com.emilia.scrolltamer.utils;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.graphics.Path;
 import android.view.accessibility.AccessibilityEvent;
 import android.util.Log;
 
@@ -8,16 +10,27 @@ public class ScrollService extends AccessibilityService {
     private static final String TAG = "ScrollTamer";
 
     @Override
-    protected void onServiceConnected() {
-        super.onServiceConnected();
-        Log.d(TAG, "!!! МАЯК ПЕРЕЗАПУЩЕН: ВИЖУ ВСЁ !!!");
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // Мы фильтруем поток: реагируем ТОЛЬКО на клик
+        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
+            Log.d(TAG, "Клик пойман! Начинаю магию...");
+            performSilkScroll();
+        }
     }
 
-    @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Получаем текстовое описание события
-        String type = AccessibilityEvent.eventTypeToString(event.getEventType());
-        Log.d(TAG, "СОБЫТИЕ: " + type);
+    private void performSilkScroll() {
+        Path path = new Path();
+        // Настройки под твой экран: ведем снизу вверх по центру
+        path.moveTo(500, 1300);
+        path.lineTo(500, 300);
+
+        GestureDescription.StrokeDescription stroke = new GestureDescription.StrokeDescription(path, 0, 500);
+        dispatchGesture(new GestureDescription.Builder().addStroke(stroke).build(), new GestureResultCallback() {
+            @Override
+            public void onCompleted(GestureDescription gestureDescription) {
+                Log.d(TAG, "Магия сработала: Скролл завершен!");
+            }
+        }, null);
     }
 
     @Override
