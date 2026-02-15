9010b156bd293e28be0d10d127be423b185ddb0f
Ghjh1 <ghjhifyu@gmail.com>
2026-02-15 02:02:56 +0200

    Silk Touch v32: Включаем прожектор (Logcat)

--- app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
@@ -1,35 +1,29 @@
 package com.emilia.scrolltamer.utils;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.GestureDescription;
-import android.graphics.Path;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
 
 public class ScrollService extends AccessibilityService {
+    private static final String TAG = "ScrollTamer";
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        Toast.makeText(this, "СЕРВИС ОЖИЛ!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "СЕРВИС ПОДКЛЮЧЕН И ГОТОВ К РАБОТЕ!");
+        Toast.makeText(this, "Связь установлена!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Проверяем клик по экрану
+        // Теперь мы будем видеть в Termux КАЖДОЕ событие
+        String eventName = AccessibilityEvent.eventTypeToString(event.getEventType());
+        Log.d(TAG, "Событие системы: " + eventName);
+
         if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
-            Toast.makeText(this, "Вижу клик! Скроллю...", Toast.LENGTH_SHORT).show();
-            performAction();
+            Log.d(TAG, "Обнаружен клик! Пытаюсь скроллить...");
         }
-    }
-
-    private void performAction() {
-        Path path = new Path();
-        path.moveTo(540, 1500);
-        path.lineTo(540, 500);
-
-        GestureDescription.StrokeDescription stroke = new GestureDescription.StrokeDescription(path, 0, 500);
-        dispatchGesture(new GestureDescription.Builder().addStroke(stroke).build(), null, null);
     }
 
     @Override
