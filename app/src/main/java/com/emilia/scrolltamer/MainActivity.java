8889fa5210f8047f07fa44551377e62b3cb47225
Ghjh1 <ghjhifyu@gmail.com>
2026-02-15 21:06:41 +0200

    v45: Переход на базовый Activity для успешной сборки

--- app/src/main/java/com/emilia/scrolltamer/MainActivity.java
+++ app/src/main/java/com/emilia/scrolltamer/MainActivity.java
@@ -1,20 +1,22 @@
 package com.emilia.scrolltamer;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView tv = findViewById(R.id.test_list_text);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 100; i++) {
-            sb.append("Строка №").append(i).append("\n");
+        if (tv != null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= 100; i++) {
+                sb.append("Строка № ").append(i).append("\n");
+            }
+            tv.setText(sb.toString());
         }
-        tv.setText(sb.toString());
     }
 }
