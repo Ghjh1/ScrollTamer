5bacfd6b87c17324c14b4f5833d0a2b583dda9d0
Ghjh1 <ghjhifyu@gmail.com>
2026-02-15 03:02:17 +0200

    v35: Полный сброс и проверка связи

--- app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
+++ app/src/main/java/com/emilia/scrolltamer/utils/ScrollService.java
@@ -1,43 +1,24 @@
 package com.emilia.scrolltamer.utils;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.GestureDescription;
-import android.graphics.Path;
 import android.view.accessibility.AccessibilityEvent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class ScrollService extends AccessibilityService {
     private static final String TAG = "ScrollTamer";
 
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Мы будем скроллить ТОЛЬКО когда ты кликаешь по экрану
-        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
-            Log.d(TAG, "Клик пойман! Запускаю шелковый скролл...");
-            performSilkScroll();
-        }
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.d(TAG, "!!! МАЯК: СВЯЗЬ УСТАНОВЛЕНА !!!");
+        Toast.makeText(this, "v35 на связи!", Toast.LENGTH_SHORT).show();
     }
 
-    private void performSilkScroll() {
-        // Координаты для Redmi Note 9C (центр экрана)
-        Path scrollPath = new Path();
-        scrollPath.moveTo(500, 1200); // Начало (снизу)
-        scrollPath.lineTo(500, 400);  // Конец (вверху)
-
-        GestureDescription.StrokeDescription stroke = new GestureDescription.StrokeDescription(scrollPath, 0, 500);
-        GestureDescription.Builder builder = new GestureDescription.Builder();
-        builder.addStroke(stroke);
-
-        dispatchGesture(builder.build(), new GestureResultCallback() {
-            @Override
-            public void onCompleted(GestureDescription gestureDescription) {
-                Log.d(TAG, "Скролл успешно выполнен!");
-            }
-            @Override
-            public void onCancelled(GestureDescription gestureDescription) {
-                Log.d(TAG, "Скролл отменен системой! (Проверь настройки безопасности)");
-            }
-        }, null);
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // Логируем ВООБЩЕ ВСЁ, чтобы понять, видит ли сервис экран
+        Log.d(TAG, "Тип события: " + event.getEventType());
     }
 
     @Override
