fe98da64491d9994674a3f2ecb79159fa849c5d4
Ghjh1 <ghjhifyu@gmail.com>
2026-02-26 18:47:17 +0200

    v146: Restored internal list + manual inputs

--- app/src/main/java/com/emilia/scrolltamer/MainActivity.java
+++ app/src/main/java/com/emilia/scrolltamer/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 import android.os.Handler;
 import com.emilia.scrolltamer.utils.ScrollService;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     private TextView debugInfo;
@@ -17,9 +17,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         debugInfo = findViewById(R.id.debug_info);
         final EditText edD = findViewById(R.id.edit_dist);
         final EditText edT = findViewById(R.id.edit_time);
+        ListView lv = findViewById(R.id.test_list);
+
+        // Тот самый список для тестов
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 1; i <= 500; i++) items.add("Строка теста №" + i + " [ПОЛУЧИТЬ 1 PX]");
+        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items));
 
         TextWatcher tw = new TextWatcher() {
             public void afterTextChanged(Editable s) {
